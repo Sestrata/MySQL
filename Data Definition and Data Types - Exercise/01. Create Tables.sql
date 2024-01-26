e0a1c0fa82b4a87913a22d60b8d588f4bb9d5f4e
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2024-01-26 22:22:47 +0200

    Create 01. Create Tables.sql

---
+++ Data Definition and Data Types - Exercise/01. Create Tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE minions(
+id INT AUTO_INCREMENT,
+name VARCHAR(80),
+age INT,
+PRIMARY KEY(id)
+);
+CREATE TABLE towns(
+town_id INT AUTO_INCREMENT,
+name VARCHAR(80),
+PRIMARY KEY(town_id)
+);
