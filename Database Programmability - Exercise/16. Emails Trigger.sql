59b1228b4014fb91c50ab7c130c683c9e3103294
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2024-02-06 10:47:31 +0200

    Create 16. Emails Trigger.sql

---
+++ Database Programmability - Exercise/16. Emails Trigger.sql
@@ -0,0 +1,35 @@
+CREATE TABLE logs(
+	log_id INT PRIMARY KEY AUTO_INCREMENT, 
+	account_id INT NOT NULL, 
+	old_sum DECIMAL(19,4), 
+	new_sum DECIMAL(19,4)
+);
+
+CREATE TRIGGER tr_update
+AFTER UPDATE
+ON accounts
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs(account_id, old_sum, new_sum) 
+    VALUES(old.id, old.balance, new.balance);
+END;
+
+CREATE TABLE notification_emails(
+	id INT PRIMARY KEY AUTO_INCREMENT, 
+	recipient INT NOT NULL, 
+	subject VARCHAR(200), 
+	body TEXT
+);
+
+CREATE TRIGGER tr_insert
+AFTER INSERT
+ON logs
+FOR EACH ROW
+BEGIN
+	INSERT INTO notification_emails(recipient, subject, body) VALUES
+    (new.account_id, 
+    CONCAT('Balance change for account: ', new.account_id),
+    CONCAT('On ', DATE_FORMAT(NOW(), '%b %d %Y at %r'),
+    ' your balance was change from ', ROUND(new.old_sum, 0), ' to ', 
+    ROUND(new.new_sum, 0), '.'));
+END;
