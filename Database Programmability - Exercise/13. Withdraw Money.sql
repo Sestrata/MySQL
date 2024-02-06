22685f240060b0d3dc201c18d65cc56bec90b4b1
Elena Yaneva <75198076+Sestrata@users.noreply.github.com>
2024-02-06 10:46:16 +0200

    Create 13. Withdraw Money.sql

---
+++ Database Programmability - Exercise/13. Withdraw Money.sql
@@ -0,0 +1,12 @@
+CREATE PROCEDURE usp_withdraw_money(acc_id INT, money_amount DECIMAL(19,4))
+BEGIN
+START TRANSACTION;
+IF((SELECT COUNT(*) FROM accounts WHERE id = acc_id) <> 1 OR 
+    ((SELECT balance FROM accounts WHERE id = acc_id) < money_amount) OR
+    money_amount < 0) 
+    THEN ROLLBACK;
+ELSE 
+	UPDATE accounts SET balance = balance - money_amount WHERE id = acc_id;
+    COMMIT;
+END IF;
+END
